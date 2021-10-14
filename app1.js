c17982d68a82c2e051cc90a39d041ec5a8e769ef
cortezalberto <cortezalberto@gmail.com>
2021-10-13 21:42:35 -0300

    Segundo Commit

--- app1.js
+++ app1.js
@@ -5,7 +5,7 @@ let archivoTareas = require('./funcionesTareas')
 // Para controlar el switch
 let accion = process.argv[2]
 
-// Un array para lojar las tareas que vienen del archivo JSON
+// Un array para alojar las tareas que vienen del archivo JSON
 let tareas = [];
 
 switch(accion) {
@@ -13,13 +13,7 @@ switch(accion) {
         // Hago el foreach
         break;
 
-    // Micro desafío 2.C
-    // ---------------
-    // Escribir el caso de crear tarea
-    // Tener en cuenta que va a llegar el nombre de la tarea como segundo argumento
-    // Tener en cuenta que la tarea se crea en esto "pendiente"
-
-
+  
     case 'crear':
          // Recibo el titulo como argumento de la consola
 
